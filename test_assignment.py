3871566c97016e2e4620204cff564648ac662a90
MrRSimple <90565682+MrRSimple@users.noreply.github.com>
2025-02-08 22:40:16 +0200

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -9,7 +9,3 @@ def test_hello():
 @pytest.mark.parametrize("input, expected", [(1, 3), (10, 12), (-4, -2)])
 def test_add_two(input, expected):
     assert assignment.add_two(input) == expected
-
-
-def test_zero_array():
-    return np.testing.assert_allclose(assignment.zero_array(100), np.zeros(shape=(100,)))
